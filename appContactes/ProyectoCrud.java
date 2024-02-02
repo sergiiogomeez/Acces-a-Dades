53fcd38b5c8ef5b3b6032de6a852e5d76d5154de
r-godet <rutxi187@gmail.com>
2024-02-02 10:14:39 +0100

    Create ProyectoCrud.java

---
+++ appContactes/ProyectoCrud.java
@@ -0,0 +1,60 @@
+rimport java.util.Scanner;
+
+    
+/* SERGIO & ROGER */
+
+
+/*GESTOR DE CONTACTOS:
+
+Requisitos:
+- llista de contactos
+- crear contactos
+- editar informacion
+- eliminar contactos
+- buscar contacto (buscador)
+
+Estructura:
+    - lista de contactos (ver nombre) //para ver el numero entrar dentro
+        - entrar en contacto (dentro)
+            - editar informacion
+                - cambiar nombre
+                - cambiar numero
+                - eliminar contacto
+            - añadir a favoritos
+        - marcador
+    - crear contacto
+        - nombre
+        - numero
+        - foto (opcional)
+    - buscar contacto (buscador)*/
+
+public class ProyectoCrud {
+    public static void main(String[] args) {
+        
+    }
+
+    //LAS TABLAS
+    private static void listaContactos(){
+        String[] contactos;
+    }
+
+    private static void verContacto(){
+
+    }
+
+    private static void editarInformacion(){
+
+    }
+    
+    private static void ponerFavoritos(){
+
+    }
+
+    private static void crearContacto(){
+
+    }
+
+    private static void buscarContacto(){
+
+    }
+}
