2a87bd1fe755359a38051131d45cef97801ddc09
r-godet <rutxi187@gmail.com>
2024-02-02 11:45:38 +0100

    d

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Helo and gfhfhgfghgfj!\n");
-
-        System.out.print("Hello and gfhfhgfghgfj!\n");
-        System.out.println("Tus muertos a caballo");
     }
 }
