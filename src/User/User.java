42c691de82d2e673e8341a70bcb04010d441d53f
r-godet <rutxi187@gmail.com>
2024-02-02 12:16:44 +0100

    users1.0

--- src/User/User.java
+++ src/User/User.java
@@ -1,11 +1,32 @@
 package User;
-
 import java.sql.SQLOutput;
-
+import java.util.Scanner;
 public class User {
-    String nombre;
-    String apellido;
-    int telefono; 
-    boolean foto;
+    static Scanner lector = new Scanner(System.in);
+    static String nombre;
+    static String apellido;
+    static int numero;
+    static String foto;
+    boolean ponerFoto;
 
+    public static void preguntas(boolean ponerFoto){
+
+        ponerFoto = false;
+
+        System.out.println("Nombre: ");
+        nombre = lector.nextLine();
+
+        System.out.println("Apellido: ");
+        apellido = lector.nextLine();
+
+        System.out.println("Numero de telefono: ");
+        numero = lector.nextInt();
+
+        System.out.println("Quieres añadir una foto? (Si o No): ");
+        foto = lector.nextLine();
+
+        if(foto.equals("Si")) {
+            ponerFoto = true;
+        }
+    }
 }
